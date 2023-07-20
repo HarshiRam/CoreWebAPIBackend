2bfb68eee844faf367c48e0821decf60fbacc4fa
HarshiRam <harshiruku@gmail.com>
2023-07-20 13:36:46 +0100

    .Net Core WebAPI Backend DB

---
+++ MyLearningApp/MyLearningApp/dbo/Tables/Products.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [ProductName] VARCHAR (50) NOT NULL,
+    [Price]       DECIMAL (18) NOT NULL,
+    [Qty]         INT          NOT NULL
+);
+
